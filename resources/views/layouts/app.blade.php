19744d0290e0e1512df60adea43235cf2d4b5869
Mildred Manrique <mildred.manrique@outlook.es>
2021-02-14 02:07:56 -0600

    De instagram a Mildred

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
                     <div><img src="/svg/instagramLogo.svg" style="height: 25px;" class="pr-3"></div>
-                    <div> Instagram </div>
+                    <div> Mildred </div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
